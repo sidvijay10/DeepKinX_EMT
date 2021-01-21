fe81db8809b161aaef2cb38f5bc42a3cc31b24c8
sidvijay10 <38486932+sidvijay10@users.noreply.github.com>
2021-01-21 12:00:28 -0500

    Update and rename Predicting_Untested_Inhibitors_sarcoma.py to Predicting_Untested_Inhibitors.py

--- Predicting_Untested_Inhibitors.py
+++ Predicting_Untested_Inhibitors.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul 12 13:37:33 2019
-
-@author: svijay
+@author: sid vijay
 """
 
 
@@ -14,8 +12,6 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import GridSearchCV
-from keras.wrappers.scikit_learn import KerasClassifier, KerasRegressor
 from sklearn.metrics import mean_squared_error
 from statistics import mean
 
@@ -29,7 +25,7 @@ response2 = response_data2['Huh7_Fzd2'].values
 dataset2["response"] = response2
 
 
-kinase_list = pd.read_csv('recursive_elimination_kinases_Huh7_Both.csv')
+kinase_list = pd.read_csv('recursive_elimination_kinases_Huh7_Fzd2.csv')
 kinase_list = kinase_list.values.tolist()
 
 kinases = []
@@ -40,13 +36,6 @@ for kinase in kinase_list:
 X = dataset2[kinases].values
 y = dataset2.iloc[:, 298].values
 
-
-# Importing the Keras libraries and packages
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-
 classifier = Sequential()
 classifier.add(Dense(units = 100, kernel_initializer = 'TruncatedNormal', activation = 'relu', input_dim = len(kinases)))
 classifier.add(Dense(units = 100, kernel_initializer = 'TruncatedNormal', activation = 'relu'))
@@ -56,22 +45,14 @@ classifier.fit(X,y, epochs=80, batch_size=44)
 
 X_predict = alldrugs2
 X_predict = X_predict[kinases]
-#X_predict = alldrugs.loc[alldrugs.index.difference(drug_list)]
 prediction_index = X_predict.index.tolist()
 X_predict = X_predict.iloc[:, 0:len(kinases)+2].values
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_predict)
-
 untested_inhibitor_prediction = pd.DataFrame(y_pred.tolist(), index = prediction_index)
-
-
-
-
 ranked_inhibitors = untested_inhibitor_prediction.sort_values(by=[0])
-
-
-ranked_inhibitors.to_excel("Ranked_inhibitors Hs578t Best Model.xlsx", sheet_name='1')
+ranked_inhibitors.to_excel("Ranked_inhibitors_Huh7_Fzd2.xlsx", sheet_name='1')
 
 
 
